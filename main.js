4a17d409204f0c46bea54c83bc7f937abbeb02a6
youjie <youjie@datastory.com.cn>
2019-03-28 15:04:38 +0800

    index.html

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ function createWindow() {
     win.loadFile('index.html')
 
     // 打开开发者工具
-    win.webContents.openDevTools()
+    // win.webContents.openDevTools()
 
     // 当 window 被关闭，这个事件会被触发。
     win.on('closed', () => {
@@ -21,12 +21,23 @@ function createWindow() {
         // 与此同时，你应该删除相应的元素。
         win = null
     })
+
+    // globalShortcut.register('CmdOrCtrl+Shift+A', captureScreen)
+    globalShortcut.register('Esc', () => {
+        if (win) {
+            win.close()
+            win = null
+        }
+    })
 }
 
 // Electron 会在初始化后并准备
 // 创建浏览器窗口时，调用这个函数。
 // 部分 API 在 ready 事件触发后才能使用。
 app.on('ready', createWindow)
+
+
+
 
 // 当全部窗口关闭时退出。
 app.on('window-all-closed', () => {
