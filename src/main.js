23d407ac3e2bfb11c3c79ddb008bcacd295b2d6f
youjie <youjie@datastory.com.cn>
2019-03-28 15:14:34 +0800

    调整

--- src/main.js
+++ src/main.js
@@ -3,13 +3,22 @@ const { app, BrowserWindow, ipcMain, globalShortcut } = require('electron')
 
 let win
 
+
+function createTray () {
+
+    tray = new Tray(`${__dirname}/src/`) // 指定图片的路径
+    // ... 其他代码
+}
+
+
 function createWindow() {
     
+    console.log(__dirname)
     // 创建浏览器窗口。
     win = new BrowserWindow({ width: 800, height: 600 })
 
     // 然后加载应用的 index.html。
-    win.loadFile('index.html')
+    win.loadFile('../index.html')
 
     // 打开开发者工具
     // win.webContents.openDevTools()
